8e2e440a28c0b21cc01e5a62fcab559b8fc1f8f5
Camilo Moran <camilomoranav@unimagdalena.edu.co>
2022-11-21 13:34:09 -0500

    primeras intetacciones front + back

--- src/components/Asamblea.js
+++ src/components/Asamblea.js
@@ -1,11 +1,81 @@
-import React, { useState } from "react";
-import { Button, Card, Col, ProgressBar, Row, Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card, Col, ProgressBar, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { vectorPreguntas } from "../utils/Info";
 import CrearPregunta from "./CrearPregunta";
 import ModificarPregunta from "./ModificarPregunta";
 
-export default function Asamblea (){
+const URI = 'http://localhost:8000/questions/'
+
+const Asamblea = () => {
+
+    const params  = useParams();
+    let idAsamblea = Number(params.idAsamblea);
+    
+    let progress = 45;
+
+    //const [estadoPreguntas, setEstadoPreguntas] = useState(preguntas);
+
+
+    const [preguntas, setPreguntas] = useState([])
+
+    let preguntasAsamblea = preguntas.filter(pregunta => pregunta.assamblies_idassamblies === idAsamblea);
+
+    useEffect (()=>{
+        getPreguntas()
+    }, [])
+
+    const getPreguntas = async () => {
+        const res = await axios.get(URI)
+        setPreguntas(res.data)
+    }
+
+    //const eliminarPreguntas = async (id) => {
+    //
+    //}
+
+    return(
+        <Row>
+            <Row>
+                <Col id="text">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-fill" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                        <path fillRule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                    </svg>&nbsp;&nbsp;/&nbsp;&nbsp;
+                    <label>Gestionar asambleas</label>
+                    &nbsp;&nbsp;/&nbsp;&nbsp;
+                    <label>Preguntas</label>
+                </Col>
+            </Row>
+            <CrearPregunta props={idAsamblea} />
+            <Col xs={{ span: 10, offset: 1 }} className="py-4">
+                <Card id="text">
+                    <Card.Header><h4>Preguntas</h4></Card.Header>
+                    <Card.Body>   
+                        {preguntasAsamblea.map((p) => 
+                            <Row key={p.id}>
+                                <Col xs={{ span: 4 }}>{p.question}</Col>                                                    
+                                <Col>
+                                    <Row>
+                                        <Col xs={{span:2}}><ModificarPregunta props={p.id}/></Col>
+                                        <Col>
+                                                        
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col><ProgressBar animated now={progress}/></Col>   
+                            </Row>
+                        )}
+                    </Card.Body>
+                </Card>
+            </Col>           
+        </Row>
+    );
+}
+
+export default Asamblea;
+
+/*export default function Asamblea (){
     const params  = useParams();
     let idAsamblea = Number(params.idAsamblea);
     let preguntas = vectorPreguntas.filter(pregunta => pregunta.idAsamblea === idAsamblea);
@@ -13,6 +83,8 @@ export default function Asamblea (){
     const [estadoPreguntas, setEstadoPreguntas] = useState(preguntas);
 
     let progress = 45;
+
+    
 
     return(
         <Row>
@@ -63,4 +135,4 @@ export default function Asamblea (){
             </Col>           
         </Row>
     );
-}
+}*/
--- src/components/Preguntas.js
+++ src/components/Preguntas.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Col, Row, Button } from "react-bootstrap";
-import { vectorPreguntas } from "../utils/Info";
 
-export default function Preguntas (){
+const URI = 'http://localhost:8000/questions/'
+
+const Preguntas = () => {
+
+    const [preguntas, setPreguntas] = useState([])
+
+    useEffect (()=>{
+        getPreguntas()
+    }, [])
+
+    const getPreguntas = async () => {
+        const res = await axios.get(URI)
+        setPreguntas(res.data)
+    }
+
+    /*const eliminarPreguntas = async (id) => {
+        axios.delete(`${URI}${id}`)
+        getPreguntas()
+    }  */  
+
     return(
         <>
             <Row>
-                {vectorPreguntas.map((p)=>(
+                {preguntas.map((p)=>(
                     <Col key={p.id} xs={{ span: 6, offset: 3 }} className="border border-secondary mb-4" id="box">
-                        <Row><Col xs={{ span: 8, offset: 2 }} className="text-center border-bottom border-dark"><h5>¿{p.pregunta}?</h5></Col></Row>
+                        <Row><Col xs={{ span: 8, offset: 2 }} className="text-center border-bottom border-dark"><h5>¿{p.question}?</h5></Col></Row>
                         <Row>
                             <div>
                                 <input type="radio" value="yes" name={p.id} /> S&iacute;. <br/>
@@ -23,6 +42,7 @@ export default function Preguntas (){
                 <Col xs={{ span: 4, offset: 4 }} className="py-4 text-center"><Button variant="primary" size="lg">Enviar</Button></Col>
             </Row>
         </>
-    );
-    
+    )    
 }
+
+export default Preguntas;
